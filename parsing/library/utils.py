77444ffbedb6dc771cf4474b396adc27ca88c37d
tigerding <zhiyuanding01@gmail.com>
2025-06-04 20:46:56 -0400

    make black formatter happy

--- parsing/library/utils.py
+++ parsing/library/utils.py
@@ -212,9 +212,7 @@ def dir_to_dict(path):
     d = {"name": os.path.basename(path)}
     if os.path.isdir(path):
         d["kind"] = "directory"
-        d["children"] = [
-            dir_to_dict(os.path.join(path, x)) for x in os.listdir(path)
-        ]
+        d["children"] = [dir_to_dict(os.path.join(path, x)) for x in os.listdir(path)]
     else:
         d["kind"] = "file"
     return d
