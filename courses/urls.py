0135e3d29e325d37c5040812d64d9a316aeb79d2
tigerding <zhiyuanding01@gmail.com>
2025-10-04 12:55:25 -0400

    update django to 4.x

--- courses/urls.py
+++ courses/urls.py
@@ -10,7 +10,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-from django.conf.urls import re_path
+from django.urls import re_path
 
 import courses.views
 
