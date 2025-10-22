fc6848a4f721f27f260cbc223b5a7ef599e78893
root <root@DESKTOP-UKV0O21.localdomain>
2025-10-22 04:42:24 -0400

    updates year UI to include 2026, and updates possible years of graduation to be up-to-date

--- static/js/redux/ui/modals/UserSettingsModal.tsx
+++ static/js/redux/ui/modals/UserSettingsModal.tsx
@@ -385,13 +385,11 @@ const UserSettingsModal = () => {
             className="select-field"
             value={{ label: userSettings.class_year, value: userSettings.class_year }}
             options={[
-              { value: 2022, label: 2022 },
-              { value: 2023, label: 2023 },
-              { value: 2024, label: 2024 },
               { value: 2025, label: 2025 },
               { value: 2026, label: 2026 },
               { value: 2027, label: 2027 },
               { value: 2028, label: 2028 },
+              { value: 2029, label: 2029}
             ]}
             menuShouldScrollIntoView={false}
             isSearchable
