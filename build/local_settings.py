7fb41e18380fc5d2489c490bb9feb4c3c223688e
root <root@DESKTOP-UKV0O21.localdomain>
2025-10-29 16:37:12 -0400

    resets PASSWORD db variable in build/local-settings.py

--- build/local_settings.py
+++ build/local_settings.py
@@ -11,7 +11,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT'],
     }
