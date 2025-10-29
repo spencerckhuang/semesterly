d5dd8bac0f97af2e26224998dc0d87ecfde8983d
root <root@DESKTOP-UKV0O21.localdomain>
2025-10-29 16:34:51 -0400

    resolves local testing issue by changing django db engine in semesterly/local-settings.py

--- build/local_settings.py
+++ build/local_settings.py
@@ -8,10 +8,10 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "postgres"),
         'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT'],
     }
